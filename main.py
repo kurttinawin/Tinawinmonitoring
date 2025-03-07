66f6f4c1a7dfd6c8935694b2fbb4f7f3815b37bb
kurttinawin <kftinawin@spmadridlaw.com>
2025-03-07 15:20:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -157,6 +157,7 @@ if uploaded_file is not None:
         'Day', 'Collector', 'Total Connected', 'Total PTP', 'Total RPC', 'PTP Amount', 'Balance Amount'
     ])
 
+    # Iterate through each collector and calculate values
     for (date, collector), collector_group in filtered_df[~filtered_df['Remark By'].str.upper().isin(['SYSTEM'])].groupby([filtered_df['Date'].dt.date, 'Remark By']):
         total_connected = collector_group[collector_group['Call Status'] == 'CONNECTED']['Account No.'].count()
         total_ptp = collector_group[collector_group['Status'].str.contains('PTP', na=False) & (collector_group['PTP Amount'] != 0)]['Account No.'].nunique()
@@ -174,7 +175,7 @@ if uploaded_file is not None:
             'Balance Amount': balance_amount,
         }])], ignore_index=True)
 
-    # Calculate total row
+    # Calculate total row for each category
     total_row = {
         'Day': 'Total',
         'Collector': '',
